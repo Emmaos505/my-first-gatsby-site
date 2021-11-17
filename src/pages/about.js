bdd8515d4db62935d45712ef205d18329f747653
Emmaos505 <emmaotero505@gmail.com>
2021-11-17 13:02:22 -0300

    part 2 of gatsby tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+
+const About = () => {
+    return (
+        <Layout pageTitle="Acerca de ... " >
+            <p> Soy la pagina de acerca de ... </p>
+        </Layout>
+    )
+};
+
+export default About;
